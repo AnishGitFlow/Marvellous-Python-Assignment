d198eafa9ed07494b03864fa2b77bbc02ffbd624
AnishGitFlow <asc14088@gmail.com>
2025-05-09 00:14:54 +0530

    Update

---
+++ Assignment_1/Assignment1_10.py
@@ -0,0 +1,8 @@
+# Write a program which accept name from user and display length of its name.
+
+# Input : Marevllous    Output : 10
+
+print("Enter your name : ")
+name = input()
+
+print(len(name))
---
+++ Assignment_1/Assignment1_8.py
@@ -0,0 +1,9 @@
+# Write a program which accepts number from user and print that number of "*" on screen.
+
+# Input : 5   Output : * * * * *
+
+print("Enter some number ")
+n = int(input())
+
+for i in range(n):
+    print("*")
