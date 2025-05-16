8e332a43458d65d6c8a1bb8e8dae0c85bae09ab7
Anish Chandlekar <163227286+AnishGitFlow@users.noreply.github.com>
2025-05-16 19:16:33 +0530

    Delete Assignment4_1.py

--- Assignment4_1.py
+++
@@ -1,9 +0,0 @@
-def main():
-    power_of_two = lambda x: x ** 2
-    
-    num = int(input("Enter a number to get its power of two: "))
-    result = power_of_two(num)
-    print(f"Output: {result}")
-
-if __name__ == "__main__":
-    main()
